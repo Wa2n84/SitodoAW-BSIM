e16e22db876f95f82515a32dc97b749f7ccdde08
Wa2n84 <153574390+Wa2n84@users.noreply.github.com>
2023-12-12 16:24:16 +0700

    Update MainController.java

--- src/main/java/com/example/sitodo/controller/MainController.java
+++ src/main/java/com/example/sitodo/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-    @GetMapping(method = RequestMethod.GET)
+    @GetMapping(method = RequestMethod.GET,value = "/")
     public String showMainPage() {
         return "redirect:/list";
     }
